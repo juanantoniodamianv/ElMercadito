941d584136dbf82b923d6b2d5f535c378e3b948b
dnd80itec <dnd_itec@hotmail.com>
2015-10-31 01:12:35 -0300

    probando
    probando

--- src/ElMercadito/ElMercadito.java
+++ src/ElMercadito/ElMercadito.java
@@ -67,14 +67,14 @@ private Sucursal unaSucursal = new Sucursal(1,"cuil","telefono","razon social");
         
          //que loco       
         Articulo art = new Articulo();
-        art.setNroArticulo(15957);
+        art.setNroArticulo(15958);
         art.setLote("22-09-2015");
         art.setFechaElabora("22-09-2015");
         art.setFechaExpira("15-11-2016");
         jpa.create(art);
         
         Caja caj = new Caja();
-        caj.setNroCaja(148);
+        caj.setNroCaja(149);
         jpaCaja.create(caj);
     }
 
